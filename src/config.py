46b11d23b0975a128f0dce001c442c6f3a564d8c
ADMIN-PC\Administrator <luongpd3460@ut.edu.vn>
2025-12-15 18:20:16 +0700

    Update database connection string for Docker SQL Server

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+
 # Configuration settings for the Flask application
 
 import os
@@ -7,24 +8,24 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a_default_secret_key'
     DEBUG = os.environ.get('DEBUG', 'False').lower() in ['true', '1']
     TESTING = os.environ.get('TESTING', 'False').lower() in ['true', '1']
-    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:Aa%40123456@127.0.0.1:1433/FlaskApiDB'
+    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:%40Bina0608@127.0.0.1:1433/QLBH'
     CORS_HEADERS = 'Content-Type'
 
 class DevelopmentConfig(Config):
     """Development configuration."""
     DEBUG = True
-    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:Aa%40123456@127.0.0.1:1433/FlaskApiDB'
+    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:%40Bina0608@127.0.0.1:1433/QLBH'
 
 
 class TestingConfig(Config):
     """Testing configuration."""
     TESTING = True
-    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:Aa%40123456@127.0.0.1:1433/FlaskApiDB'
+    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:%40Bina0608@127.0.0.1:1433/QLBH'
 
 
 class ProductionConfig(Config):
     """Production configuration."""
-    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:Aa%40123456@127.0.0.1:1433/FlaskApiDB'
+    DATABASE_URI = os.environ.get('DATABASE_URI') or 'mssql+pymssql://sa:%40Bina0608@127.0.0.1:1433/QLBH'
 
     
 template = {
