75ebdcbf8204fa497d9483f557e95a5a82279c29
ADMIN-PC\Administrator <luongpd3460@ut.edu.vn>
2025-12-25 13:42:16 +0700

    Update unit_model.py

---
+++ src/infrastructure/models/unit_model.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from infrastructure.databases.base import Base
+from datetime import datetime
+class Unit(Base):
+    __tablename__ = 'units'
+    __table_args__ = {'extend_existing': True}  # Thêm dòng này
+
+    id = Column(Integer, primary_key=True) # Cho phép NULL
+    name =Column(String(50),nullable=True)
+    description = Column(String(255), nullable=True)
+    status = Column(String(50), nullable=False)
+    created_at = Column(DateTime,default=datetime.utcnow,nullable=False)
+    updated_at = Column(DateTime,default=datetime.utcnow,onupdate=datetime.utcnow,nullable=False) 
