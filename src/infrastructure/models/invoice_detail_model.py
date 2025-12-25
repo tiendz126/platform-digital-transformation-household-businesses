c03f5d317c7c3651d9f23a5795044461622ca4b5
ADMIN-PC\Administrator <luongpd3460@ut.edu.vn>
2025-12-25 14:40:03 +0700

    Update invoice_detail_model.py

---
+++ src/infrastructure/models/invoice_detail_model.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from infrastructure.databases.base import Base
+from datetime import datetime
+class Invoice(Base):
+    __tablename__ = 'invoices'
+    __table_args__ = {'extend_existing': True}  # Thêm dòng này
+
+    id = Column(Integer, primary_key=True) # Cho phép NULL
+    invoice_id=Column(Integer, ForeignKey("invoices.id"),nullable=True) # Hóa đơn mua
+    product_unit_id=Column(Integer,ForeignKey("product_unit.id"),nullable=True) # Hóa đơn bán
+    discount_total=Column(Integer,nullable=True)
+    vat=Column(Integer,nullable=False)
+    discount=Column(Integer, nullable=False) 
+    price=Column(Integer, nullable=True)
+    description = Column(String(255), nullable=True)
+    status = Column(String(50), nullable=False)  # Draft / Confirm / Delete 
+    created_at = Column(DateTime,default=datetime.utcnow,nullable=False)
+    updated_at = Column(DateTime,default=datetime.utcnow,onupdate=datetime.utcnow,nullable=False) 
