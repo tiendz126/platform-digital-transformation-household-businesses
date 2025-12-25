10e2637cea89729c60254ed4cdf7fe7f5833d9d1
ADMIN-PC\Administrator <luongpd3460@ut.edu.vn>
2025-12-25 14:55:37 +0700

    Update All

--- src/infrastructure/models/Import_detail.py
+++ src/infrastructure/models/Import_detail.py
@@ -1,18 +1,16 @@
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from infrastructure.databases.base import Base
 from datetime import datetime
-class InvoiceDetail(Base):
-    __tablename__ = 'invoice_detail'
+class ImportDetail(Base):
+    __tablename__ = 'import_detail'
     __table_args__ = {'extend_existing': True}  # Thêm dòng này
 
     id = Column(Integer, primary_key=True) # Cho phép NULL
-    invoice_id=Column(Integer, ForeignKey("invoices.id"),nullable=False) # chi tiết hóa đơn bán
-    product_unit_id=Column(Integer,ForeignKey("product_unit.id"),nullable=False) # Sản phẩm giá theo đơn vị
-    vat=Column(Integer,nullable=False)
-    discount=Column(Integer, nullable=True) 
-    price=Column(Integer, nullable=True)
-    description = Column(String(255), nullable=True)
+    invoice_id=Column(Integer, ForeignKey("invoices.id"),nullable=False) # Chi tiết hóa đơn mua
+    product_id=Column(Integer,ForeignKey("products.id"),nullable=False)
+    unit_id=Column(Integer, ForeignKey("units.id"),nullable=False)
     quantity=Column(Integer,nullable=False)
+    description = Column(String(255), nullable=True)
     status = Column(String(50), nullable=False)  # Draft / Confirm / Delete 
     created_at = Column(DateTime,default=datetime.utcnow,nullable=False)
     updated_at = Column(DateTime,default=datetime.utcnow,onupdate=datetime.utcnow,nullable=False) 
