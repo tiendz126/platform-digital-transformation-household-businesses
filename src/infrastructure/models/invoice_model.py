cb0dd53dcfccbf1d5477a6b23f75eb6da111f6da
ADMIN-PC\Administrator <luongpd3460@ut.edu.vn>
2025-12-25 14:00:25 +0700

    Update invoice_model.py

---
+++ src/infrastructure/models/invoice_model.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from infrastructure.databases.base import Base
+from datetime import datetime
+class Warehouse(Base):
+    __tablename__ = 'warehouses'
+    __table_args__ = {'extend_existing': True}  # Thêm dòng này
+
+    id = Column(Integer, primary_key=True) # Cho phép NULL
+    household_id=Column(Integer, ForeignKey("households.id"),nullable=False)
+    seller_id=Column(Integer, ForeignKey("sellers.id"),nullable=True) # Hóa đơn mua
+    customer_id=Column(Integer,ForeignKey("customers.id"),nullable=True) # Hóa đơn bán
+    invoice_type=Column(String(50,nullable=False)) 
+    discount_total=Column(Integer,nullable=True)
+    vat_total=Column(Integer,nullable=False)
+    total_amount=Column(Integer, nullable=False) # (quantity * price) - discount_total + vat_total
+    description = Column(String(255), nullable=True)
+    status = Column(String(50), nullable=False)  # Draft / Confirm / Delete 
+    created_by= Column(String(50),nullable=True) # Role Employee, Owner 
+    updated_by= Column(String(50),nullable=True) # Role Employee, Owner
+    created_at = Column(DateTime,default=datetime.utcnow,nullable=False)
+    updated_at = Column(DateTime,default=datetime.utcnow,onupdate=datetime.utcnow,nullable=False) 
