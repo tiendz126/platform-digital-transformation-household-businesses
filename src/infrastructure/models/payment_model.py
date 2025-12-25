ab67ab3a7981000546b87a4aac500a4d41a08754
ADMIN-PC\Administrator <luongpd3460@ut.edu.vn>
2025-12-25 15:01:48 +0700

    payment_model.py

---
+++ src/infrastructure/models/payment_model.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from infrastructure.databases.base import Base
+from datetime import datetime
+class Payment(Base):
+    __tablename__ = 'payments'
+    __table_args__ = {'extend_existing': True}  # Thêm dòng này
+
+    id = Column(Integer, primary_key=True) # Cho phép NULL
+    invoice_id=Column(Integer, ForeignKey("invoices.id"),nullable=False)
+    method_id=Column(Integer,ForeignKey("paymentmethods.id"),nullable=False)
+    amount=Column(Integer, nullable=False)
+    description = Column(String(255), nullable=True)
+    status = Column(String(50), nullable=False)
+    created_by= Column(String(50),nullable=True)
+    updated_by= Column(String(50),nullable=True)
+    created_at = Column(DateTime,default=datetime.utcnow,nullable=False)
+    updated_at = Column(DateTime,default=datetime.utcnow,onupdate=datetime.utcnow,nullable=False) 
