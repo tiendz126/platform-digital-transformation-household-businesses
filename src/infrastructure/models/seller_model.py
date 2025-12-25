0f82b2d73de8567ba46a018706e4cacb36a9bae2
ADMIN-PC\Administrator <luongpd3460@ut.edu.vn>
2025-12-25 12:56:43 +0700

    Update seller_model.py

---
+++ src/infrastructure/models/seller_model.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from infrastructure.databases.base import Base
+from datetime import datetime
+class Household(Base):
+    __tablename__ = 'households'
+    __table_args__ = {'extend_existing': True}  # Thêm dòng này
+
+    id = Column(Integer, primary_key=True) # Cho phép NULL
+    tax_code=Column(String(12),nullable=True,unique=True) # Mã số thuế hộ kinh doanh bằng số CCCD người đại diện
+    name =Column(String(50),nullable=True)
+    phone =Column(String(20),nullable=True)
+    address =Column(String(255),nullable=True)
+    description = Column(String(255), nullable=True)
+    status = Column(String(50), nullable=False)
+    created_by= Column(String(50),nullable=True)
+    updated_by= Column(String(50),nullable=True)
+    created_at = Column(DateTime,default=datetime.utcnow,nullable=False)
+    updated_at = Column(DateTime,default=datetime.utcnow,onupdate=datetime.utcnow,nullable=False) 
