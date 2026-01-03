0db04ee6f25c39681de0f2ca9ff25c8e7fda5485
ADMIN-PC\Administrator <luongpd3460@ut.edu.vn>
2026-01-03 23:45:15 +0700

    Update debt_record_model.py

--- src/infrastructure/models/debt_record_model.py
+++ src/infrastructure/models/debt_record_model.py
@@ -7,9 +7,9 @@ class DebtRecord(Base):
 
     id = Column(Integer, primary_key=True) # Cho phép NULL
     customer_id=Column(Integer, ForeignKey("customers.id"),nullable=False)
-    invoice_id=Column(Integer, ForeignKey("invoices.id"),nullable=True,unique=True)
-    payment_id=Column(Integer, ForeignKey("payments.id"),nullable=True,unique=True)
-    debit_amount=Column(Numeric(10), nullable=False) # nợ
+    invoice_id=Column(Integer, ForeignKey("invoices.id"),nullable=True,unique=True) # Mã chứng từ hóa đơn nợ
+    payment_id=Column(Integer, ForeignKey("payments.id"),nullable=True,unique=True) # Mã chứng từ thanh toán trả nợ
+    debit_amount=Column(Numeric(10), nullable=True) # nợ
     credit_amount=Column(Numeric(10), nullable=True)  # trả
     balance=Column(Numeric(10), nullable=True)  # trả - nợ
     description = Column(String(255), nullable=True) # Diễn giải
