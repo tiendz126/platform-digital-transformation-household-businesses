0e441efa49cbb8fe3568d92e6dc6cbab70aa20e0
ADMIN-PC\Administrator <luongpd3460@ut.edu.vn>
2026-01-09 20:11:03 +0700

    Update file name

--- src/infrastructure/models/Import_detail_model.py
+++ src/infrastructure/models/Import_detail_model.py
@@ -1,13 +1,12 @@
-from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy import Column, ForeignKey, Integer
 from infrastructure.databases.base import Base
-from datetime import datetime
+
 class ImportDetail(Base):
     __tablename__ = 'import_details'
-    __table_args__ = {'extend_existing': True}  # Thêm dòng này
+    __table_args__ = {'extend_existing': True}
 
-    id = Column(Integer, primary_key=True, autoincrement=True) # Cho phép NULL
-    import_id=Column(Integer,ForeignKey("import_receipts.id"),nullable=False)
-    product_id=Column(Integer,ForeignKey("products.id"),nullable=False)
-    unit_id=Column(Integer, ForeignKey("units.id"),nullable=False)
-    quantity=Column(Integer,nullable=False)
-   
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    import_id = Column(Integer, ForeignKey("import_receipts.id"), nullable=False)
+    product_id = Column(Integer, ForeignKey("products.id"), nullable=False)
+    unit_id = Column(Integer, ForeignKey("units.id"), nullable=False)
+    quantity = Column(Integer, nullable=False)
